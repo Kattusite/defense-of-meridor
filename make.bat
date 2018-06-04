8a1fe6d88eb21aa34996fbf59523e58470abfb84
kattusite <kattusite@gmail.com>
2018-06-04 17:31:34 -0400

    Added manifest and jar creation
    You can now make the app using make.bat, as expected. It will recompile all sources and put them into a nice executable jar

--- make.bat
+++ make.bat
@@ -1,2 +1,13 @@
-javac src/firework/*.java -d bin/firework/
-javac src/meridor/*.java -d bin/meridor/
+REM Compile the source files into class files
+javac -cp . src/firework/*.java src/meridor/*.java -d bin/
+
+REM Create an executable jar file
+REM jar cmf IOM.mf IOM.jar bin/firework/* bin/meridor/* MeriImages
+cd bin
+jar cmf ../IOM.mf IOM.jar firework/* meridor/* ../MeriImages
+cd ..
+
+jar cmf IOM.mf IOM.jar MeriImages -C bin . 
+
+REM Wait for input
+PAUSE
